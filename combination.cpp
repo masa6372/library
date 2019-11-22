d6bbf00bad1e7182412f40e808d4e07270977381
masa6372 <57652830+masa6372@users.noreply.github.com>
2019-11-23 01:19:44 +0900

    Create combination.cpp

---
+++ combination.cpp
@@ -0,0 +1,23 @@
+//MAX,MODは問題ごとに変わる
+long long fac[MAX],finv[MAX],inv[MAX];
+
+//テーブルを作る前処理
+template<class T>
+T com(){
+	fac[0]=fac[1]=1;
+	finv[0]=finv[0]=1;
+	inv[1]=1;
+	for(int i=2;i<MAX;i++){
+		fac[i]=fac[i-1]*i%MOD;
+		inv[i]=MOD-inv[MOD%i]*(MOD/i)%MOD;
+		finv[i]=finv[i-1]*inv[i]%MOD;
+	}
+}
+
+//二項計算 nCk
+template<class T>
+T com(int n,int k){
+	if(n<k)return 0;
+	if(n<0||k<0)return 0;
+	return fac[n]*(finv[k]*finv[n-k]%MOD)%MOD;
+}
