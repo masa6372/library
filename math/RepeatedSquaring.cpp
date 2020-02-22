d5b8f3a43059d4374525a24a8528597cabc51ab1
masa6372 <57652830+masa6372@users.noreply.github.com>
2020-02-23 08:01:25 +0900

    Create RepeatedSquaring.cpp

---
+++ math/RepeatedSquaring.cpp
@@ -0,0 +1,10 @@
+*/繰り返し２乗法/*
+ll func(ll n,ll m,ll p){
+        if(p==0)return 1;
+        if(p%2==0){
+                ll t=func(n,m,p/2);
+                return t*t%m;
+        }else{
+                return n*func(n,m,p-1)%m;
+        }
+}
