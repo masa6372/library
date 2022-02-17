4b6227c68905079b484212ad2c87167909cdcee1
masa6372 <57652830+masa6372@users.noreply.github.com>
2022-02-18 05:27:45 +0900

    Create factorial.py

---
+++ math/factorial.py
@@ -0,0 +1,12 @@
+from collections import Counter
+
+def factorial(num):
+    arr = []
+    for i in range(2, num**(1/2)+1):
+        while (num % i == 0):
+            arr.append(i)
+            num //= i
+    if num > (int(num**(1/2))):
+        arr.append(num)
+    return arr
+c = Counter(factorial(n)) 
